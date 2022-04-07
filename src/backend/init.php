b71db96d4e0971e25fa5b0ef7623ffda913ddfb4
b3cker <b3ckerdev@protonmail.com>
2022-04-06 21:28:20 -0300

    Add files via upload

---
+++ src/backend/init.php
@@ -0,0 +1,9 @@
+<?php
+
+include("./src/backend/Store.php");
+include("./src/backend/BuyBots.php");
+
+include("./src/backend/utils/Session.php");
+include("./src/backend/utils/GetMethod.php");
+
+?>
